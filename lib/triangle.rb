7e932d084832aab7674d1e23e473ca94f304b1c1
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-19 18:22:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,6 +7,17 @@ class Triangle
     @c = c
   end	
   
+  #   it 'knows that triangles violating triangle inequality are illegal' do
+  #   expect{Triangle.new(1, 1, 3).kind}.to raise_error(Triangle::TriangleError)
+  # end
+
+  # it 'knows that triangles violating triangle inequality are illegal 2' do
+  #   expect{Triangle.new(2, 4, 2).kind}.to raise_error(Triangle::TriangleError)
+  # end
+
+  # it 'knows that triangles violating triangle inequality are illegal 3' do
+  #   expect{Triangle.new(7, 3, 2).kind}.to raise_error(Triangle::TriangleError)
+  # end
   def kind 
     if @a <= 0 || @b <= 0 || @c <= 0 
       raise TriangleError
