5b6b61c2538d6ba4e5e596a7bf25b9455a5212bf
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-19 16:22:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,8 @@
 class Triangle
   
   def initialization(length_one,length_two, length_three)
-    @equilateral = equilateral
-    @isosceles = isosceles
-    @scalene = scalene
   end
+  
+  
+  
 end
